2c9dc0cea22e2b38751a2685b17ccd1d4e29c131
liqiqi <742538662@qq.com>
2018-01-03 09:47:26 +0800

    seccend  commit

---
+++ js/loading.js
@@ -0,0 +1,12 @@
+/**
+ */
+// iframe 引入右侧内容区
+window.onload = initPage();
+function initPage()
+{
+    var objLoading = document.getElementById("loading-box");
+    if (objLoading != null)
+    {
+         objLoading.style.display = "none";
+    }
+}
